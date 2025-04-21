1f1b987158559177dd94fb26970871fd9a389049
SebastienRe <sebastien.re@icube-tc.com>
2025-04-21 17:51:53 +0200

    [ADD] Implémentation du modèle de propriété immobilière avec accès et permissions

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Estate",
+    'depends': [
+        'base',
+    ],
+    'category': 'Tutorials/Estate',
+    'application': True,
+    'installable': True,
+    "data": [
+        'security/ir.model.access.csv',
+    ],
+}
